881601363ee3d734965a0f9e83ef34f008d0f5fe
Bethoveen <bethoveen.todino@bridgetechnologypartners.com>
2013-08-31 15:03:31 +0800

    Fixing facebook login

--- app/routes/user.js
+++ app/routes/user.js
@@ -15,7 +15,7 @@ module.exports = function(passport, LocalStrategy, _ , userModel, validator) {
 	};
 
 	self.setAuthenticationFacebook = function(accessToken, refreshToken, profile, done) {
-		console.log('OK got it ' + JSON.parse(profile));
+		console.log('OK got it ' + profile.id + '  , ' + profile.emails[0].value + ' , ' + profile.displayName);
 		//done(null, user);
 		/*userModel.findUserById(profile.id, profile, function(err, user) {
 			if (err instanceof Err) { return done(err); }
